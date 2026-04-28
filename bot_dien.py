3bccc835706d87b6c7147ea7e2a8edf4f450f1ba
quangvinhhy-svg <quangvinhhy@gmail.com>
2026-04-28 14:43:41 +0700

    Update bot_dien.py

--- bot_dien.py
+++ bot_dien.py
@@ -148,7 +148,12 @@ def format_message(data_dict):
 # ===== MAIN =====
 def main():
     print("🚀 Đang chạy bot...")
+# ===== CHỈ GỬI MỖI 1 TIẾNG =====
+    now_vn = datetime.utcnow() + timedelta(hours=7)
 
+    if now_vn.minute not in [0, 1, 2]:
+        print("⏭️ Skip")
+        return
     data = get_data()
     print("✅ Đã lấy dữ liệu")
 
